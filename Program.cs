9478f9e964eb1f4769605d5beee70ea6a276f5f3
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-01-27 15:29:58 -0400

    Upload v0.1

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿/*Ejercicio 1 y ejercicio 5: */
+System.Console.WriteLine("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+System.Console.WriteLine($"Hola, " + nombre + ", Usted esta cursando Aplicada I.");
